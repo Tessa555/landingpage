9fa85a965a4672cf99fea57e1f313da4d8e072c4
Tessa555 <69504607+Tessa555@users.noreply.github.com>
2021-01-27 16:49:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    // toggle menubar/navbar script
+    $('.menu-btn').click(function(){
+        $(' .navbar .menu').toggleClass("active");
+        $(' .menu-btn i').toggleClass("active");
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
